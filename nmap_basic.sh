d717f2a53503f47d0c59f5a064085dc46da648cb
Daniel Donda <danieldonda@msn.com>
2022-01-07 08:07:54 -0500

    Alterado o script do nmap

--- nmap_basic.sh
+++ nmap_basic.sh
@@ -6,13 +6,13 @@
 # echo -n "Porta aberta que quer descobrir :"
 # read porta
 
-rede="192.168.0.0/24"
-porta="80"
+rede=$1
+porta=$2
 
 clear
-echo .............................................
-echo Procurando hosts na rede $rede com a porta $porta aberta
-echo ---------------------------------------------
-# nmap -sS -vv -p 80 192.168.0.0/24 | grep "Discovered open port" | awk '{print$
-nmap -sS -vv -p $porta $rede | grep "Discovered open port" | awk '{print $6}'
+echo ------------------------------------------------------------- 
+echo Procurando hosts na rede $1 com a porta $2 aberta
+echo -------------------------------------------------------------
+# EXEMPLO DO COMANDO: nmap -sS -vv -p 80 192.168.0.0/24 | grep "Discovered open port" | awk '{print$
+nmap -sS -vv -p $2 $1 | grep "Discovered open port" | awk '{print $6}'
 echo --------------------------------------------
